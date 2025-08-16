86b36fc058e60029a5fb626a00409011c4a80849
Ondřej Berčík <ondrej.bercik@gmail.com>
2025-08-16 14:51:45 +0200

    fix: change font of Radix UI theme and set new theme (black and gold)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,14 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
+import './theme-config.css';
 import '@radix-ui/themes/styles.css';
 import Navbar from './Navbar';
-import { Theme } from '@radix-ui/themes';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const inter = Inter({
   subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: '--font-inter',
 });
 
 export const metadata: Metadata = {
@@ -27,10 +23,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Theme>
+      <body className={`${inter.variable} antialiased`}>
+        <Theme accentColor='gold' radius='large' appearance='dark'>
           <Navbar />
           <main className='p-6'>{children}</main>
         </Theme>
